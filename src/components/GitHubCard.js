5c25145e1a7acb3907b73399dbdde9a64130b47b
ISxaNI <jasonliu99999@yahoo.com>
2024-04-25 18:44:51 -0700

    Finished

---
+++ src/components/GitHubCard.js
@@ -0,0 +1,20 @@
+import React from "react"
+import './GitHubCard.css';
+
+function GitHubCard() {
+    return (
+        <div className="App">
+            <h2>
+                Hello World!
+            </h2>
+            <div>
+                <a href="https://github.com/ISxaNI">
+                    Heres a link to my Github Page!
+                </a>
+            </div>
+        </div>
+    );
+  }
+  
+
+export default GitHubCard;
